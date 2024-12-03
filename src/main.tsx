5446a2ed073b358137b1907ad7e7fa8078d8fad2
katas8n1 <kirka6061512@icloud.com>
2024-12-03 20:12:05 +0100

    added dummy shift

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+const a = 23;
+console.log(a);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
