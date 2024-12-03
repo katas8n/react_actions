6cf4d1a4f1bfc646f241450f711c1444d08e7aba
katas8n1 <kirka6061512@icloud.com>
2024-12-03 20:53:06 +0100

    Added deployment

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
     <h1>Hello!</h1>
+    <h2>World</h2>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
